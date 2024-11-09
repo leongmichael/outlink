574729c5e5391758025943f184482582ba4ca2b4
Michael Leong <michaelleong1229@gmail.com>
2024-11-09 14:53:01 -0800

    Backend (#1)
    * server
    
    * formatting

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+import analyticsRoutes from "./routes/analytics.js";
+
+// app
+const app = express();
+
+//middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+dotenv.config();
+
+//routes
+app.use("/analytics", analyticsRoutes);
+
+//connect
+mongoose.connect(process.env.CONNECTION_URL);
+const db = mongoose.connection;
+db.once("open", () => console.log(`Connected to database`));
+
+//listener
+const port = 8080;
+const server = app.listen(port, () =>
+  console.log(`Server is running on port ${port}`),
+);
