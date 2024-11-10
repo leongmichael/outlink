4ccd621754f2a7f5198b4089dc2add75027eacd9
leongmichael <michaelleong1229@gmail.com>
2024-11-09 18:35:10 -0800

    revert

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     admin: {
       userId: {
         type: Number,
-        required: false,
+        required: true,
       },
       email: {
         type: String,
