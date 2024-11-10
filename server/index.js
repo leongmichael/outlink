a49f71afd18b41e41e3b725fee0540a8e3152934
Michael Leong <michaelleong1229@gmail.com>
2024-11-10 12:13:41 -0800

    periodic (#13)

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const userRoutes = require("./routes/userRoutes.js");
 const eventRoutes = require("./routes/eventRoutes.js");
+const { startPeriodicEventGeneration } = require('./controllers/eventController');
 // app
 const app = express();
 
@@ -29,3 +30,6 @@ const port = 8080;
 const server = app.listen(port, () =>
   console.log(`Server is running on port ${port}`)
 );
+
+// Add this after your server is initialized but before it starts listening
+startPeriodicEventGeneration();
