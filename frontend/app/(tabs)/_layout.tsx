2fb479ced0b8a843e5fa22051a37694ad8a19299
Peter Wang <plwang@usc.edu>
2024-11-09 21:44:09 -0800

    main page with swiping

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function TabLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: true }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       {/* Add any other screens that were previously tabs */}
     </Stack>
   );
